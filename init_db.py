cfc836ac86ee8e4966f79966adb246b78893335d
Jake Kovinsky <jakbruin@g.ucla.edu>
2026-04-12 15:39:52 -0700

    general clean up

--- init_db.py
+++ init_db.py
@@ -8,17 +8,6 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Greer just shitted', 'Content for the first post')
-            )
-
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Why Frank sucks cock', 'Content for the second post')
-            )
-
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Why porn is good', 'It makes the goo come out')
-            )
 
 connection.commit()
 connection.close()

fb961a44779c05ef39139502cfa81f96c8291ae6
Jake Kovinsky <jakbruin@g.ucla.edu>
2026-04-13 22:21:41 -0700

    added improved error handling for llm client

--- llm.py
+++ llm.py
@@ -8,13 +8,20 @@ GEMINI_API_KEY = os.getenv("GOOGLE_GEMINI_API_KEY")
 
 def gen_forecast_summary(forecast):
     client = genai.Client(api_key=GEMINI_API_KEY)
+    client_response = ""
+    try:
 
-    response = client.models.generate_content(
-        model="gemini-3.1-flash-lite-preview",
-        config={
-            "system_instruction" : "Only output a summary of the weather forecast"
-        },
-        contents=f"Provide a concise summary of the weather forecast: {forecast}"
-    )
-
-    return response.text
+        response = client.models.generate_content(
+            model="gemini-3.1-flash-lite-preview",
+            config={
+                "system_instruction" : "Only output a summary of the weather forecast"
+            },
+            contents=f"Provide a concise summary of the weather forecast: {forecast}"
+        )
+        client_response = response.text
+        return client_response
+    except Exception as error:
+        print(f"Unexpected: {error}")
+    
+    return client_response
+    
